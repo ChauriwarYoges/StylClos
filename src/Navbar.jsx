fe0f2bd74aa8eb0662ea84e8320c924d7b9c834d
ChauriwarYoges <71879968+ChauriwarYoges@users.noreply.github.com>
2022-01-16 12:21:30 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
                                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon"></span>
                                 </button>
-                                {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                                     <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                                         <li className="nav-item">
                                             <NavLink style={({ isActive }) => isActive ? activeStyle : inactiveStyle} to="Home">
@@ -53,7 +53,7 @@ const Navbar = () => {
                                             </NavLink>
                                         </li>
                                     </ul>
-                                {/* </div> */}
+                                </div>
                             </div>
                         </nav>
                     </div>
