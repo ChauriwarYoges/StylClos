4d4aae0b8f397defe2db57cc58116ee53fcb0674
ChauriwarYoges <yogeschauriwar@gmail.com>
2022-04-12 10:48:33 +0530

    updating project till admin page completion

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Collection from '../Collections/Collection';
+import Header from './Header';
+
+function Home() {
+
+    
+    return (<>
+        
+        <Header />
+
+        <Collection />
+
+    </>);
+}
+
+export default Home;
---
+++ src/Services/seller.service.js
@@ -0,0 +1,27 @@
+import httpClient from '../http-common';
+
+const getAll = () => {
+  return httpClient.get('/product/list');
+};
+
+const get = (id) => {
+  return httpClient.get('/user/'+id);
+};
+
+const update = (data) => {
+  return httpClient.put('', data);
+};
+
+const signin = (data) => {
+  return httpClient.post('/user/signin', data);
+}
+
+const signup = (data) => {
+  return httpClient.post('/user/signup', data);
+}
+
+const getProductBasedOnSeller = (id) => {
+  return httpClient.get('/seller/getproducts/' + id);
+} 
+
+export default { getAll, get, update, signin, signup, getProductBasedOnSeller };
